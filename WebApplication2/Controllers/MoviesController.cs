c129f65af1d1a2a3a66fdfc2a595daee123d5963
Jatin Kumawat <jatinkumawat20@gmail.com>
2022-10-10 10:15:47 +0530

    Basic Clear

---
+++ WebApplication2/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Id = 1,Title="Avengers" };
+            return View(movie);
+        }
+    }
+}
---
+++ WebApplication2/Models/Movies.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication2.Models
+{
+    public class Movies
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
